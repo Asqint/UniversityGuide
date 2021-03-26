0b1b485f714beaa8ed6c90beec4850190df6adfe
Asqint <romapyzhiv@gmail.com>
2021-03-26 10:21:41 +0300

    Initial commit

---
+++ src/main/java/com/grsu/guide/UniversityGuideApplication.java
@@ -0,0 +1,13 @@
+package com.grsu.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityGuideApplication.class, args);
+	}
+
+}
