e8623a3da5ebf1c17c5ae1cbebb9ee5cb8010b81
Babich <super_bob_16@mail.ru>
2021-04-23 14:16:05 +0300

    Add class Audio

---
+++ src/main/java/com/grsu/guide/domain/Audio.java
@@ -0,0 +1,32 @@
+package com.grsu.guide.domain;
+
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Audio {
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name="audio")
+    private String audio;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Page page;
+
+    public Audio(long id,String audio, Page page){
+        this.id = id;
+        this.audio = audio;
+        this.page = page;
+    }
+
+    public Audio() {}
+}
+
+
