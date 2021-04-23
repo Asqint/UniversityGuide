0c90e125b6e8ed9e59db5aeb25602358aa74148d
Babich <super_bob_16@mail.ru>
2021-04-23 14:23:09 +0300

    Add GetPage/GetAllPages functions in PageService

---
+++ src/main/java/com/grsu/guide/service/PageService.java
@@ -0,0 +1,24 @@
+package com.grsu.guide.service;
+
+import com.grsu.guide.domain.Page;
+import com.grsu.guide.repository.PageRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PageService {
+   private final PageRepository pageRepository;
+
+    public PageService(PageRepository pageRepository) {
+        this.pageRepository = pageRepository;
+    }
+
+    public Page GetPage (String namePage){
+        return pageRepository.findByNamePage(namePage);
+    }
+
+    public Iterable<Page> GetAllPages(){
+        return pageRepository.findAll();
+    }
+
+}
+
