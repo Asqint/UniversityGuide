7316b2282c1150639e9ff8835d7ad087bf4729f5
Babich <super_bob_16@mail.ru>
2021-04-23 14:16:27 +0300

    Add class Text

---
+++ src/main/java/com/grsu/guide/domain/Text.java
@@ -0,0 +1,30 @@
+package com.grsu.guide.domain;
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+public class Text {
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name="text")
+    private String text;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Page page;
+
+    public Text(long id,String text, Page page){
+        this.id = id;
+        this.text = text;
+        this.page = page;
+    }
+
+    public Text() {}
+}
