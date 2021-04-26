a827752239e48d2bc3413365737c8024f3c04fee
Asqint <romapyzhiv@gmail.com>
2021-04-26 21:27:52 +0300

    Navbar added

--- src/main/java/com/grsu/guide/controllers/PageController.java
+++ src/main/java/com/grsu/guide/controllers/PageController.java
@@ -1,17 +1,27 @@
 package com.grsu.guide.controllers;
 
+import com.grsu.guide.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PageController {
 
     @GetMapping("/home")
-        public String home(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model){
-        model.addAttribute("name",name);
+        public String home(Model model){
+        Page pageOne = new Page();
+        List<String> list = new ArrayList<>();
+        list.add("First");
+        list.add("Second");
+        list.add("Third");
+        list.add("Fourth");
+        list.add("Fifth");
+        model.addAttribute("list", list);
         return "home";
-
     }
 }
