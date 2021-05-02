5d5cc16e83cf40cac242670800bc4f809bb080a6
Babich <super_bob_16@mail.ru>
2021-05-02 20:22:47 +0300

    Add Element

---
+++ src/main/java/com/grsu/guide/domain/Element.java
@@ -0,0 +1,30 @@
+package com.grsu.guide.domain;
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Element {
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "value")
+    private String value;
+
+    public Element(String type,String value){
+        this.type = type;
+        this.value = value;
+    }
+
+    public Element() {
+
+    }
+}
