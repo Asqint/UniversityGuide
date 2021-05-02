a233e617510ab49399eef52bb2026f59aa89ac9e
Babich <super_bob_16@mail.ru>
2021-05-02 20:31:24 +0300

    Add GetPage

--- src/main/java/com/grsu/guide/controllers/PageController.java
+++ src/main/java/com/grsu/guide/controllers/PageController.java
@@ -1,17 +1,31 @@
 package com.grsu.guide.controllers;
 
+import com.grsu.guide.domain.Element;
+import com.grsu.guide.domain.Page;
+import com.grsu.guide.service.PageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @Controller
 public class PageController {
 
-    @GetMapping("/home")
-        public String home(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model){
-        model.addAttribute("name",name);
-        return "home";
+    private final PageService pageService;
 
+    @Autowired
+    public PageController(PageService pageService){
+        this.pageService = pageService;
+    }
+
+    @GetMapping("/{namePage}")
+        public String GetPage(@PathVariable(value = "namePage") String namePage, Model model){
+       Page page = pageService.GetPage(namePage);
+       List<Page> pages = (List<Page>) pageService.GetAllPages();
+        model.addAttribute("page", page);
+        model.addAttribute("pages", pages);
+        return "main";
     }
 }
