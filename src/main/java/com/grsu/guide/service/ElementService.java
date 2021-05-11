f9a6c9eb810fdb43a5168406fc3afaa88b895262
BabichAndrei <super_bob_16@mail.ru>
2021-05-11 20:46:06 +0300

    Add UploadElement

--- src/main/java/com/grsu/guide/service/ElementService.java
+++ src/main/java/com/grsu/guide/service/ElementService.java
@@ -4,9 +4,13 @@ import com.grsu.guide.domain.Element;
 import com.grsu.guide.domain.Page;
 import com.grsu.guide.repository.ElementRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class ElementService {
@@ -22,6 +26,18 @@ public class ElementService {
     }
 
     public void DeleteElement(Long id){ elementRepository.deleteById(id);}
+
+    public String UploadElement(MultipartFile file, String uploadPath) throws IOException {
+        File uploadFolder = new File(uploadPath);
+        if(!uploadFolder.exists()){
+            uploadFolder.mkdir();
+        }
+        String uuidFile = UUID.randomUUID().toString();
+        String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+        file.transferTo(new File(uploadPath + "/" + resultFilename));
+        return resultFilename;
+    }
 
 
 
