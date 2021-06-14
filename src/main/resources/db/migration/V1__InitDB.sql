6dc54bf4920052f1452a4e887723fb0a4cccb683
Asqint <romapyzhiv@gmail.com>
2021-06-14 14:05:42 +0300

    Database script initialization

---
+++ src/main/resources/db/migration/V1__InitDB.sql
@@ -0,0 +1,27 @@
+create sequence hibernate_sequence start with 1 increment by 1;
+
+create table element (
+    id bigint not null,
+    type varchar(255) not null,
+    value varchar(2048) not null,
+    page_id bigint not null,
+    primary key (id)
+);
+
+create table page (
+    id bigint not null,
+    name_page varchar(255) not null,
+    primary key (id)
+);
+
+create table user (
+    id bigint not null,
+    email varchar(255),
+    message varchar(2048),
+    user_name varchar(255),
+    primary key (id)
+);
+
+alter table element
+    add constraint element_page_fk
+    foreign key (page_id) references page;
