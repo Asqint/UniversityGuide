369bac3ae3179d7971b96ec6f48aaf204dbac379
BabichAndrei <super_bob_16@mail.ru>
2021-06-14 12:38:30 +0300

    Added User

---
+++ src/main/java/com/grsu/guide/domain/User.java
@@ -0,0 +1,33 @@
+package com.grsu.guide.domain;
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name="user_name")
+    private String userName;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="message")
+    private String message;
+
+    public User(String user_name,String email,String message){
+        this.userName = user_name;
+        this.email = email;
+        this.message = message;
+    }
+
+    public User() {}
+}
