a82360f0b5d4d15f11bacb7f11a3e95cd8c64f4b
BabichAndrei <super_bob_16@mail.ru>
2021-06-14 14:34:14 +0300

    Added UserRepository

---
+++ src/main/java/com/grsu/guide/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.grsu.guide.repository;
+
+import com.grsu.guide.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User,Long> {
+    User findByUserName(String namePage);
+
+}
