cc1bfd536138caf22202db543acf73e24a7e0bd1
Asqint <romapyzhiv@gmail.com>
2021-06-22 17:25:13 +0300

    Search space allowed

--- src/main/resources/static/script/search.js
+++ src/main/resources/static/script/search.js
@@ -1,11 +1,14 @@
 window.onload = () => {
     let search = document.querySelector('#search');
     search.oninput = function () {
-        let value = this.value.trim();
+        let value = this.value.trimLeft();
         if(value === '') {
             location.replace("/search");
         }
         else{
+            if(value.includes(' ')) {
+                value = value.replace(/ /g,'+');
+            }
             location.replace("/search?searchRequest="+value);
         }
     }
