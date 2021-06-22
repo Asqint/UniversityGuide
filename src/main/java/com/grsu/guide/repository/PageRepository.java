2d8b147dcc884bc648f32ab3da052b20e7c945fe
BabichAndrei <super_bob_16@mail.ru>
2021-06-22 14:34:27 +0300

    Changed findByUrlPage to findById

--- src/main/java/com/grsu/guide/repository/PageRepository.java
+++ src/main/java/com/grsu/guide/repository/PageRepository.java
@@ -4,9 +4,11 @@ import com.grsu.guide.domain.Page;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface PageRepository extends CrudRepository<Page,Long> {
-    Page findByUrlPage(String urlPage);
+    Optional<Page> findById(Long id);
 
 }
