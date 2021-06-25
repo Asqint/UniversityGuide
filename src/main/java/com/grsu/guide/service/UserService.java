1190a78a2a63cc7067a0f078b35b5f518f90489f
BabichAndrei <super_bob_16@mail.ru>
2021-06-25 18:06:36 +0300

    Added findUserByUserName and removed saveUser

--- src/main/java/com/grsu/guide/service/UserService.java
+++ src/main/java/com/grsu/guide/service/UserService.java
@@ -2,18 +2,27 @@ package com.grsu.guide.service;
 
 import com.grsu.guide.domain.User;
 import com.grsu.guide.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
 
+
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public void saveUser(User user){
-        userRepository.save(user);
+    public User findUserByUserName(String name){
+        return userRepository.findByUserName(name);
     }
+
+
 }
