bf2e63a8c9d920fbf15fab6a454ec789b820751a
BabichAndrei <super_bob_16@mail.ru>
2021-06-28 18:24:54 +0300

    Added findById and deleteTemplate

--- src/main/java/com/grsu/guide/service/TemplateService.java
+++ src/main/java/com/grsu/guide/service/TemplateService.java
@@ -5,6 +5,7 @@ import com.grsu.guide.repository.TemplateRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TemplateService {
@@ -18,6 +19,10 @@ public class TemplateService {
     public List<Template> findAllTemplatesById(Long id){
         return templateRepository.findTemplatesByUserId(id);
     }
+
+    public Optional<Template> findById(Long id){ return templateRepository.findById(id);}
+
+    public void deleteTemplate(Long id){ templateRepository.deleteById(id);}
 
     public void addTemplate(Template template){ templateRepository.save(template);}
 }
