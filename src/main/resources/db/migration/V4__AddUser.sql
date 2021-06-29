b8389769129b1bd3997b9b0063aadd4e35fa4337
BabichAndrei <super_bob_16@mail.ru>
2021-06-29 18:46:42 +0300

    Changed to default admin

--- src/main/resources/db/migration/V4__AddUser.sql
+++ src/main/resources/db/migration/V4__AddUser.sql
@@ -1,2 +1,1 @@
-insert into users (id, user_name, password) values(10,'Andrey', '1');
-insert into users (id, user_name, password) values(13,'Roman', '2');
+insert into users (id, user_name, password) values(20,'admin', 'admin');
